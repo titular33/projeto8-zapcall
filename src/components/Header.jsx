cf47935b29cde12e5232e896a54fd66218c9ada2
titular33 <amorim.pedro@engenharia.ufjf.br>
2022-03-21 15:34:03 -0300

    Apagando os files que deram errado

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <img src="./img/logo.png" />
-            <p>ZapRecall</p>
-        </header>
-    )
-}
--- src/index.jsx
+++
@@ -1,4 +0,0 @@
-import { render } from "react-dom";
-import App from "./components/App";
-const element = document.querySelector(".root")
-render(<App />, element);
